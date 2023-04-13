a29cdf4f755caba1a86d4c3264f93b7b02a8b937
CherepokBear <sibgufk684@gmail.com>
2023-04-13 19:36:47 +0300

    first commit

---
+++ hw-3.js
@@ -0,0 +1,91 @@
+let password = 'пароль';
+let pws = prompt("Введите пароль");
+if (pws === pasword) {
+    console.log("Пароль введен верно");
+} else {
+    console.log("Пароль введен неправильно");
+}
+
+let c = 35;
+if (c > 0 && c < 10) {
+    console.log("верно");
+} else {
+    console.log("неверно");
+}
+
+let d = 35;
+let e = 25;
+if (d || e > 100) {
+    console.log("верно");
+} else {
+    console.log("неверно");
+}
+
+let a = "2";
+let b = "3";
+let num = Number(a + b);
+alert(num);
+
+let jan = "1";
+let feb = "2";
+let mar = "3";
+let apr = "4";
+let may = "5";
+let jun = "6";
+let jul = "7";
+let aug = "8";
+let sep = "9";
+let oct = "10";
+let nov = "11";
+let dec = "12";
+
+let month = prompt("Ведите номер");
+
+if (month === "jan") {
+    console.log("Январь")
+} else if (month === "feb") {
+    console.log("Февраль")
+} else if (month === "mar") {
+    console.log("Март")
+} else if (month === "apr") {
+    console.log("Апрель")
+} else if (month === "may") {
+    console.log("Май")
+} else if (month === "jun") {
+    console.log("Июнь")
+} else if (month === "jul") {
+    console.log("Июль")
+} else if (month === "aug") {
+    console.log("Август")
+} else if (month === "sep") {
+    console.log("Сентябрь")
+} else if (month === "oct") {
+    console.log("Октябрь")
+} else if (month === "nov") {
+    console.log("Ноябрь")
+} else if{
+    console.log("Декабрь")
+}else {
+    console.log("No release")
+}
+
+let funtic = prompt("Пожалуйста, введите любое число");
+if (funtic === Number) {
+    console.log(funtic);
+} else {
+    console.log(NaN);
+}
+if (funtic % 2 == 0) {
+    alert("Число четное");
+} else {
+    alert("Число нечетное");
+}
+
+let clientOS = 0;
+let clietAndroid = 1;
+if (clientOS) {
+    alert("Установите версию приложения для iOS по ссылке");
+} else (clientAndroid){
+    alert("Установите версию приложения для Android по ссылке");
+}
+
