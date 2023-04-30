28ca11769410b55613160d2244a32fd9808ccf87
CherepokBear <sibgufk684@gmail.com>
2023-04-30 23:31:19 +0300

    1

--- hw-8.js
+++ hw-8.js
@@ -1,45 +1,28 @@
-//1
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
-];
-console.log(people.sort(function (a, b) {
-    if (a.age > b.age) {
-        return 1;
-    }
-    if (a.age < b.age) {
-        return -1;
-    }
-    return 0;
-}));
-
+// //1
+// const people = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+// ];
+// console.log(people.sort((a, b) => a.age - b.age));
 
 //2
 
-function isPositive() {
-    for (var key in people) {
-        // этот код будет вызван для каждого свойства объекта
-        // ..и выведет имя свойства и его значение
-
-        alert("Ключ: " + key + " значение: " + people[key]);
-    }
+function isPositive(item) {
+    return item >= 0;
 }
 function isMale() {
-    // console.log('');
-    // return
+    return people(gender === 'male')
 }
+
 function filter(arr, ruleFunction) {
-    // const result = arr.filter(item => item > 0);   //.sort((a, b) => a - b);
-    // console.log(result);
-    const output = [];
-
+    function ruleFunction() { arr.filter(item => item > 0); }
+    const result = [];
     for (let i = 0; i < arr.length; i++) {
-        output.push(ruleFunction(arr[i]));
+        result.push(ruleFunction(arr[i]));
     }
-
-    return output;
+    return result;
 }
 
 console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
@@ -54,35 +37,32 @@ const people = [
 console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
 
 
-
-//3
-// повторить с интервалом 2 секунды
-let timerId = setInterval(() => alert('tick'), 2000);
-// остановить вывод через 5 секунд
-setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+// //3
+// let timerId = setInterval(() => alert('tick'), 2000);
+// setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
 
 
-//4
-function delayForSecond(callback) {
-    setTimeout(delayForSecond, 1000);
-    callback();
-}
+// //4
+// function delayForSecond(callback) {
+//     setTimeout(delayForSecond, 1000);
+//     callback();
+// }
 
-delayForSecond(function () {
-    console.log('Привет, Глеб!');
-})
+// delayForSecond(function () {
+//     console.log('Привет, Глеб!');
+// })
 
-//5
-function delayForSecond(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if (cb) { cb(); }
-    }, 1000)
-}
+// //5
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+//         if (cb) { cb(); }
+//     }, 1000)
+// }
 
-function sayHi(name) {
-    console.log(`Привет, ${name}!`);
-}
+// function sayHi(name) {
+//     console.log(`Привет, ${name}!`);
+// }
 
-delayForSecond(() => sayHi('Глеб'))
+// delayForSecond(() => sayHi('Глеб'))
 
